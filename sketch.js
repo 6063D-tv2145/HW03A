e0c3c2c7c6933d064af86aff745e00fe4617baa7
Thao Chi Vu <tv2145@nyu.edu>
2023-09-26 12:51:02 -0400

    added 3 nested for loops. One to create small dotted pattern and two to create a checkerboard pattern with larger circles

--- sketch.js
+++ sketch.js
@@ -1,6 +1,35 @@
+let smallDiam = 10
+let smallSpacing = 40
+
+let largeDiam = 25;
+let largeSpacing = 80
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(500, 750);
+  background(255, 218, 22);
+
 }
 
 function draw() {
+  for (let x = 40; x < width; x += smallSpacing) {
+    for (let y = 30; y < height; y += smallSpacing) {
+      fill ("black")
+      ellipse(x, y, smallDiam, smallDiam);
+    }
+  }
+  translate(0, 0)
+  for (let x = 0; x < width; x += largeSpacing) {
+    for (let y = largeDiam; y < height; y += largeSpacing) {
+      fill ("black")
+      ellipse(x, y, largeDiam, largeDiam);
+    }
+  }
+  translate(40, 40)
+  for (let x = 0; x < width; x += largeSpacing) {
+    for (let y = largeDiam; y < height; y += largeSpacing) {
+      fill ("black")
+      ellipse(x, y, largeDiam, largeDiam);
+    }
+  }
+
 }
